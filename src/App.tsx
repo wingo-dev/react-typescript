2cbdc93a8622a8e284424c09d2a9d5dba410576b
wingo-dev <rukshanlancer@gmail.com>
2023-06-09 14:18:04 +0800

    component work

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Todos from './components/todos';
 function App() {
   return (
     <div>
+      <Todos/>
     </div>
   );
 }
---
+++ src/components/todos.tsx
@@ -0,0 +1,10 @@
+const Todos = () =>{
+    return(
+    <ul>
+        <li>React Leran</li>
+        <li>Typescript Learn</li>
+    </ul>
+    );
+}
+
+export default Todos;
