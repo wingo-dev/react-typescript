8e7dcfca006c97c7f6b8067b084aaaa64a399f7b
wingo-dev <rukshanlancer@gmail.com>
2023-06-13 17:28:56 +0800

    todo class add

--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -1,6 +1,12 @@
 import React from "react"
-
+import Todo from "../modules/todo";
 const Todos = () =>{
+    const todos = [
+        new Todo('Learn React'),
+        new Todo('Type Script React'),
+    ];
+    console.log(todos);
+    
     return(
     <ul>
         <li>React Leran</li>
